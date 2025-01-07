f4b523e4ec088c3b97d453e25ecb7020fde53557
jinjin <swj0507@gmail.com>
2025-01-07 17:31:12 +0900

    [programmers] added solution5

--- src/main/java/com/programmers/programmers/basic/Basic_1.java
+++ src/main/java/com/programmers/programmers/basic/Basic_1.java
@@ -145,9 +145,31 @@ public class Basic_1 {
         return ans;
     }
 
+    public void solution5(){
+        int[] arr = {0,1,2,3,4,5};
+        int[] query = {4,1,2};
+
+        int [] ans = {};
+
+        for(int i=0; i < query.length; i++){
+            if(i % 2 == 0 ){
+                arr = Arrays.copyOfRange(arr, 0, query[i]+1 );
+                System.out.println(Arrays.toString(arr));  // ans 배열 출력
+
+            }else{
+                arr = Arrays.copyOfRange(arr, query[i], arr.length);
+                System.out.println(Arrays.toString(arr));  // ans 배열 출력
+
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));  // ans 배열 출력
+    }
+
     public static void main(String[] args) {
         Basic_1 a = new Basic_1();
-        System.out.println(a.solution4(6,6,3,3));
+//        System.out.println(a.solution4(6,6,3,3));
+        a.solution5();
     }
 
 
